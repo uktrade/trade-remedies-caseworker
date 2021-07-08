a6db684212db7ef1f55cc0cae5ba228b595d7e05
davecharles <22290851+davecharles@users.noreply.github.com>
2021-07-08 17:25:16 +0100

    fix: black.

--- trade_remedies_caseworker/users/views.py
+++ trade_remedies_caseworker/users/views.py
@@ -20,7 +20,6 @@ logger = logging.getLogger(__name__)
 
 
 class UserBaseTemplateView(LoginRequiredMixin, TemplateView, TradeRemediesAPIClientMixin):
-
     def validate_password(self, request, user=None):
         """Validate password.
 
