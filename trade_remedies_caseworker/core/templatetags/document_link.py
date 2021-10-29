f37c6ce7c0da09f2009de0b6e03a400026af21d9
Dave Charles <22290851+davecharles@users.noreply.github.com>
2021-10-29 14:55:32 +0100

    Fix: TRLST-475 refactor and remove av scan logic
    As part of the effort to remove all the API related anti-virus scan logic this is a set of related tidy up changes:
    
    - There is no concept of an "unsafe" document anymore as malware is rejected at point of upload, updated download links accordingly.
    - django_chunk_upload_handlers package expects an AWS_REGION setting (although this was not a functional issue), added to stop package logging complaint.
    - There we some tests that were not being executed properly due to invalid pytest config. The tests needed fixing up too.

--- trade_remedies_caseworker/core/templatetags/document_link.py
+++ trade_remedies_caseworker/core/templatetags/document_link.py
@@ -13,22 +13,11 @@ Usage:
 @register.simple_tag()
 def document_link(document, with_link=True, with_confidential_mark=False):
     _link = ["", escape(document.get("name")), ""]
-    safe = document.get("safe")
-    if safe and with_link:
+    if with_link:
         _link[
             0
         ] = f"""<a href="/document/{document['id']}/download/" class="link" target="_blank">"""
         _link[2] = "</a>"
-    elif (safe is None) and with_link:
-        _link[0] = (
-            '<i class="icon icon-amber-warning correct margin-right-5px" '
-            'title="This file has not yet been scanned for viruses"></i>'
-        )
-    elif safe is False:
-        _link[0] = (
-            '<i class="icon icon-skull correct margin-right-5px" '
-            'title="This file is infected with a virus"></i>'
-        )
-    if safe and with_confidential_mark:
+    if with_confidential_mark:
         _link.append(" (confidential)" if document.get("confidential") else " (non-confidential)")
     return mark_safe("".join(_link))
