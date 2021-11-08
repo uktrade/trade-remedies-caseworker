99270b95f1e82a460f700654c63361ccc0e7ffeb
davecharles <22290851+davecharles@users.noreply.github.com>
2021-11-08 17:03:14 +0000

    fix: only enable links when user is in correct group.

--- trade_remedies_caseworker/cases/views.py
+++ trade_remedies_caseworker/cases/views.py
@@ -45,6 +45,7 @@ from core.constants import (
     SECURITY_GROUP_TRA_ADMINISTRATOR,
     SECURITY_GROUPS_TRA_ADMINS,
     SECURITY_GROUP_ORGANISATION_OWNER,
+    SECURITY_GROUPS_TRA_TOP_ADMINS,
     SUBMISSION_TYPE_QUESTIONNAIRE,
     SUBMISSION_TYPE_APPLICATION,
     SUBMISSION_NOTICE_TYPE_INVITE,
@@ -143,14 +144,15 @@ class CasesView(LoginRequiredMixin, TemplateView, TradeRemediesAPIClientMixin):
             ],
         }
 
-        can_view_only = False
-        for group in request.user.groups:
-            if group == "TRA Investigator":
-                can_view_only = True
-
+        # Contend with nonsense that should be rectified in V2.
+        # If we requested this user's cases, `case["user_case"]` flag will be
+        # present and set to True. Otherwise, to be able to navigate to a case
+        # the user must be member of SECURITY_GROUPS_TRA_TOP_ADMINS.
+        users_groups = set(request.user.groups)
+        allowed_groups = set(SECURITY_GROUPS_TRA_TOP_ADMINS)
+        can_navigate = bool(users_groups.intersection(allowed_groups))
         for case in cases:
-            if case["stage"]["name"] == "Created" and can_view_only:
-                case["view_only"] = True
+            case["can_navigate"] = can_navigate or case.get("user_case", False)
 
         template_name = self.template_name if panel_layout else "cases/cases_old.html"
         body_class = "full-width kill-footer" if panel_layout else "full-width"
