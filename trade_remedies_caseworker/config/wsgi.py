8836bd86d846b3ba3097b4371bceac5f16fc501b
nboyse <natasha.boyse@digital.trade.gov.uk>
2022-03-18 12:23:28 +0000

    Adjust directory layout

--- trade_remedies_caseworker/config/wsgi.py
+++ trade_remedies_caseworker/config/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "trade_remedies_caseworker.config.settings.local")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
 
 # from whitenoise.django import DjangoWhiteNoise  # noqa
 
