bde9f58fce2274518cb70e53c688df166f868350
Johnny <knowyourlines@outlook.com>
2022-03-29 10:10:45 +0100

    editable field highlighted in pink

--- trade_remedies_caseworker/core/utils.py
+++ trade_remedies_caseworker/core/utils.py
@@ -232,7 +232,7 @@ def parse_notify_template(template, values):
             value = value.replace("<", "&lt;").replace(">", "&gt;")
             template = template.replace(
                 f"(({key}))",
-                f'<br/><input name="deadline" class="notify-tag" title="{key}" size="20" style="border: none" \
+                f'<br/><input name="deadline" class="notify-tag" title="{key}" size="20" style="border: none; background: pink" \
                     value="{value}" placeholder="31 January 1999">\
                         </input><br/>',
             )
