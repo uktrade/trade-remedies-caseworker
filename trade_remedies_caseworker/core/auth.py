7d8a9d8286a5c8f67ee1af4495a9e4f09bbd386e
Steve Holden <steve.holden@digital.trade.co.uk>
2022-12-07 11:28:17 +0000

    Trivial simplifications, 1

--- trade_remedies_caseworker/core/auth.py
+++ trade_remedies_caseworker/core/auth.py
@@ -1,24 +1,30 @@
+"""
+auth.py: Caseworker client authentication.
+"""
 from django.contrib.auth.models import User
 from trade_remedies_client.client import Client
 
 
 class TransientUser(User):
     def has_group(self, groups):
-        if not isinstance(groups, list):
+        if isinstance(groups, str):
             groups = [groups]
         return any([grp in self.groups for grp in groups])
 
 
 class AuthenticationBackend:
     def authenticate(self, request, username=None, password=None, **kwargs):
-        response_data = Client().authenticate(username, password)
-        if response_data and response_data.get("token"):
-            request.session["token"] = response_data["token"]
-            request.session["user"] = response_data["user"]
-        user = TransientUser(username=email, token=response_data["token"])  # noqa: F821
+        rd = Client().authenticate(username, password)
+        if rd and rd.get("token"):
+            request.session["token"] = rd["token"]
+            request.session["user"] = rd["user"]
+        # If the following F821 suppression is legitimate, please indicate in
+        # a replacement for this comment how executing the code avoids
+        # raising a NameError exception.
+        user = TransientUser(username=email, token=rd["token"])  # noqa: F821
         user.is_authenticated = True
         user.backend = self
-        user.groups = response_data["groups"]
+        user.groups = rd["groups"]
         return user
 
     def has_perm(self, perm, **kwargs):
