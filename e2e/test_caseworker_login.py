025a7814c518064be64b99bfa0aee3585447b381
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-12-12 09:54:18 +0000

    fixing test dependencies

--- e2e/test_caseworker_login.py
+++ e2e/test_caseworker_login.py
@@ -6,6 +6,9 @@ BASE_URL = get_base_url()
 
 @retry()
 def test_public_login(page):
-    page.goto(f"{BASE_URL}/")
+    page.goto(f"{BASE_URL}")
     expect(page.get_by_role("link", name="Trade Remedies Authority")).to_be_visible()
-    expect(page.get_by_role("link", name="Log in")).to_be_visible()
+    expect(page.get_by_role("button", name="Log in")).to_be_visible()
+    expect(page.get_by_role("link", name="Forgotten password")).to_be_visible()
+    expect(page.get_by_label("Email")).to_be_visible()
+    expect(page.get_by_label("Password")).to_be_visible()
