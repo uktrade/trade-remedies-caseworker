b39c8bf576425497596222e8f011344080940689
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2025-04-04 11:30:43 +0100

    Compile assets in build pipeline

--- .copilot/image_build_run.sh
+++ .copilot/image_build_run.sh
@@ -3,4 +3,5 @@
 # Exit early if something goes wrong
 set -e
 
-# Add commands below to run inside the container after all the other buildpacks have been applied
+cd trade_remedies_caseworker
+python manage.py collectstatic --noinput
