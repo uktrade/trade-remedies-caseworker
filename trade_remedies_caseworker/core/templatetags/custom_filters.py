5cc7bc475798e5d1dd7fee7078558d4669961b68
Uka Osim <uka.osim@digital.trade.gov.uk>
2025-05-19 13:23:44 +0100

    fixing pagination

--- trade_remedies_caseworker/core/templatetags/custom_filters.py
+++ trade_remedies_caseworker/core/templatetags/custom_filters.py
@@ -129,22 +129,3 @@ def split(str):
 @register.filter
 def splitlines(str):
     return (str or "").splitlines()
-
-
-@register.filter
-def get_range(total, start=1, end=None):
-    """
-    Return a range of numbers for pagination.
-    Usage:
-        {% for i in total|get_range:start,end %}
-            {{ i }}
-        {% endfor %}
-
-    Example:
-        {% for i in 10|get_range:1,5 %}
-            {{ i }}  # outputs 1,2,3,4,5
-        {% endfor %}
-    """
-    if end is None:
-        end = total + 1
-    return range(int(start), min(int(end) + 1, int(total) + 1))
---
+++ trade_remedies_caseworker/core/templatetags/page_range.py
@@ -0,0 +1,18 @@
+from core.templatetags import register
+
+
+@register.simple_tag
+def page_range(total, start=1, end=None):
+    """
+    Return a range of integers for pagination.
+    
+    Usage:
+        {% page_range total start end as range %}
+        {% for i in range %}
+            {{ i }}
+        {% endfor %}
+    """
+    if end is None:
+        end = int(total)
+    
+    return range(int(start), min(int(end) + 1, int(total) + 1))
