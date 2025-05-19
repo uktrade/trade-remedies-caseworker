b1e1c2952eadeb29664a6c2d0994a29dad248001
Uka Osim <uka.osim@digital.trade.gov.uk>
2025-05-19 11:39:18 +0100

    lint fix

--- trade_remedies_caseworker/core/templatetags/get_range.py
+++ trade_remedies_caseworker/core/templatetags/get_range.py
@@ -1,5 +1,6 @@
 from core.templatetags import register
 
+
 @register.simple_tag
 def get_range(total, start=1, end=None):
     """
@@ -8,7 +9,7 @@ def get_range(total, start=1, end=None):
         {% for i in total|get_range:start,end %}
             {{ i }}
         {% endfor %}
-        
+
     Example:
         {% for i in 10|get_range:1,5 %}
             {{ i }}  # outputs 1,2,3,4,5
